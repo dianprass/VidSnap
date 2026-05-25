99c502ac9ff62e841d9427e32fd4d0620649bbdc
dianprass <dianprass44@gmail.com>
2026-05-25 11:22:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const os = require('os');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // ── Middleware ──────────────────────────────────────────
 app.use(cors());
